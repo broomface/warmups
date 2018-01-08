b5d3c855f087214e0822afd4913883575f723d5f
nrfi@sbcglobal.net <scott.m.rushford@gmail.com>
2018-01-08 10:09:59 -0700

    updated

--- Fetch/main.js
+++ Fetch/main.js
@@ -1,16 +1,30 @@
-var fetch = require('node-fetch');
-
-function plainFetch() {
-  // this fetch grabs the boulder sub reddit
-  fetch('https://www.reddit.com/r/boulder.json').then(function(response) {
-    // the json method on the response object also returns a promise
-    return response.json();
-  }).then(function(json) {
-    // so any additional json processing must happen in another then
-    console.log(json.data.children[0]);
-  });
-}
-
-plainFetch();
+// var fetch = require('node-fetch');
+//
+// function plainFetch() {
+//   // this fetch grabs the boulder sub reddit
+//   fetch('https://www.reddit.com/r/boulder.json').then(function(response) {
+//     // the json method on the response object also returns a promise
+//     return response.json();
+//   }).then(function(json) {
+//     // so any additional json processing must happen in another then
+//     console.log(json.data.children[2]);
+//   });
+// }
+//
+// plainFetch();
 
 // How can we use async / await to rewrite the function above?
+
+
+var fetch = require('node-fetch');
+
+async function asyncFetch () {
+  // this fetch grabs the boulder sub reddit
+  let response = await fetch('https://www.reddit.com/r/boulder.json')
+    // get this info from Boulder's reddit.  Once you are done, then move on
+  let json = await response.json();
+    // Take the response above and 
+    console.log(json.data.children[0]);
+}
+
+asyncFetch();
